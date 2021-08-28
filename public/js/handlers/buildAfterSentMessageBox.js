5de1e4697ebcb41f5def706f65b00a11a06e71b9
Riccardo <riccardo@neutag.com>
2021-08-28 23:01:48 +0200

    further trial of bugfixing #afterSentMessageBox

--- public/js/handlers/buildAfterSentMessageBox.js
+++ public/js/handlers/buildAfterSentMessageBox.js
@@ -1,52 +1,55 @@
-import {ElementWithAttributes} from '/sc/generalFunctions/ElementWithAttributesConstructor.js';
-import {addGlobalVar} from '/sc/generalFunctions/addGlobalVar.js';
+import { ElementWithAttributes } from "/sc/generalFunctions/ElementWithAttributesConstructor.js";
+import { addGlobalVar } from "/sc/generalFunctions/addGlobalVar.js";
 
+function buildAfterSentMessageBox(orderRequestMessageStatus) {
+  body.afterSentMessageBox = addGlobalVar({
+    afterSentMessageBox: new ElementWithAttributes(
+      "div",
+      { attribute: "id", value: "afterSentMessageBox" },
+      body
+    ),
+  });
 
-
-function buildAfterSentMessageBox(orderRequestMessageStatus){
-
-    body.afterSentMessageBox = addGlobalVar({afterSentMessageBox:
-            new ElementWithAttributes('div',
-                {attribute: "id", value: "afterSentMessageBox"},
-                body
-            )
-        }
-    )
-
-    body.afterSentMessageBox.x = new ElementWithAttributes('div',
-    {attribute: "class", value:"X"},
+  body.afterSentMessageBox.x = new ElementWithAttributes(
+    "div",
+    { attribute: "class", value: "X" },
     body.afterSentMessageBox
-    )
+  );
 
-    body.afterSentMessageBox.x.content = new ElementWithAttributes('div',
-    {attribute: "class", value:"X"},
+  body.afterSentMessageBox.x.content = new ElementWithAttributes(
+    "div",
+    { attribute: "class", value: "X" },
     body.afterSentMessageBox.x
-    )
+  );
 
-    body.afterSentMessageBox.sentBoxContent = new ElementWithAttributes('div',
-    {attribute: "id", value:"sentBoxContent"},
+  body.afterSentMessageBox.sentBoxContent = new ElementWithAttributes(
+    "div",
+    { attribute: "id", value: "sentBoxContent" },
     body.afterSentMessageBox
-    )
+  );
 
-    body.afterSentMessageBox.x.content.innerHTML = "X"
+  body.afterSentMessageBox.x.content.innerHTML = "X";
 
-    document.querySelector("#afterSentMessageBox").style.visibility = "visible"
-
-    body.afterSentMessageBox.x.onclick = ()=>document.querySelector("#afterSentMessageBox").style.visibility = "hidden";
-
-
-    if(orderRequestMessageStatus === "sent"){
-        body.afterSentMessageBox.sentBoxContent.innerHTML = "Anfrage wurde erfoglreich versendet.\nJemand wird sich in Kürze bei Ihnen melden. "
-
+  document.addEventListener("click", (event) => {
+    if (
+      event.target ==
+      document.querySelector("#afterSentMessageBox > div.X > div")
+    ) {
+      document
+        .querySelectorAll("#afterSentMessageBox")
+        .forEach((e) => e.remove());
     }
+  });
 
-    if(orderRequestMessageStatus === "failed"){
-        body.afterSentMessageBox.sentBoxContent.innerHTML = "Hoppla. Etwas ist beim Absenden schiefgelaufen.\nBitte rufen Sie uns einfach direkt unter der Nummer an: +49 1575 2760564"
-    }
+  if (orderRequestMessageStatus === "sent") {
+    body.afterSentMessageBox.sentBoxContent.innerHTML =
+      "Anfrage wurde erfoglreich versendet.\nJemand wird sich in Kürze bei Ihnen melden. ";
+  }
 
-    
+  if (orderRequestMessageStatus === "failed") {
+    body.afterSentMessageBox.sentBoxContent.innerHTML =
+      "Hoppla. Etwas ist beim Absenden schiefgelaufen.\nBitte rufen Sie uns einfach direkt unter der Nummer an: +49 1575 2760564";
+  }
 }
 
-export{buildAfterSentMessageBox}
-
-
+export { buildAfterSentMessageBox };
